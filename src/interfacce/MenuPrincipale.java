c6c406891d0fd5b2d5b7258a5737182fb6bb7294
fabiodellinfante <fabiodellinfante@san-18.lan>
2017-10-18 17:03:21 +0200

    oo

--- src/interfacce/MenuPrincipale.java
+++ src/interfacce/MenuPrincipale.java
@@ -15,12 +15,10 @@ public class MenuPrincipale {
 		frame.getContentPane().setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JComboBox<Torneo> lista = new JComboBox<Torneo>();
 		JPanel panel1=new JPanel();
 		JLabel label1=new JLabel("Seleziona il torneo:");
 		panel1.add(label1);
 		frame.add(panel1,BorderLayout.NORTH);
-		frame.add(lista,BorderLayout.CENTER);
 		
 		frame.pack();
 		frame.setVisible(true);
